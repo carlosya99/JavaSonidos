5ef33786ac7bd15d54f49d962157d19a09687595
Carlos Abia <carlosabiamer@gmail.com>
2018-04-10 10:23:48 +0200

    Añadido wow y gif thug life.

--- src/codigo/VentanaSonidos.java
+++ src/codigo/VentanaSonidos.java
@@ -143,6 +143,7 @@ public class VentanaSonidos extends javax.swing.JFrame {
         jButton61 = new javax.swing.JButton();
         jButton62 = new javax.swing.JButton();
         jButton63 = new javax.swing.JButton();
+        jButton64 = new javax.swing.JButton();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenuItem1 = new javax.swing.JMenuItem();
@@ -814,6 +815,17 @@ public class VentanaSonidos extends javax.swing.JFrame {
         });
         getContentPane().add(jButton63, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 720, 148, 50));
 
+        jButton64.setBackground(new java.awt.Color(51, 204, 255));
+        jButton64.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        jButton64.setForeground(new java.awt.Color(255, 255, 255));
+        jButton64.setText("Wow");
+        jButton64.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                jButton64MousePressed(evt);
+            }
+        });
+        getContentPane().add(jButton64, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 720, 148, 50));
+
         jMenu1.setText("Gifs");
 
         jMenuItem1.setPreferredSize(new java.awt.Dimension(80, 40));
@@ -1210,7 +1222,7 @@ public class VentanaSonidos extends javax.swing.JFrame {
 
     private void jButton58MousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton58MousePressed
          reproduce ("/sonidos/ThugLife.wav");
-         muestraGif("snoopDog3.gif");
+         muestraGif("snoopDog.gif");
     }//GEN-LAST:event_jButton58MousePressed
 
     private void jMenuItem15MousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jMenuItem15MousePressed
@@ -1237,6 +1249,11 @@ public class VentanaSonidos extends javax.swing.JFrame {
     private void jButton63MousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton63MousePressed
         reproduce("/sonidos/queNo.wav");
     }//GEN-LAST:event_jButton63MousePressed
+
+    private void jButton64MousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton64MousePressed
+        reproduce("/sonidos/wow.wav");
+        muestraGif("wow.gif");
+    }//GEN-LAST:event_jButton64MousePressed
 
     /**
      * @param args the command line arguments
@@ -1329,6 +1346,7 @@ public class VentanaSonidos extends javax.swing.JFrame {
     private javax.swing.JButton jButton61;
     private javax.swing.JButton jButton62;
     private javax.swing.JButton jButton63;
+    private javax.swing.JButton jButton64;
     private javax.swing.JButton jButton7;
     private javax.swing.JButton jButton8;
     private javax.swing.JButton jButton9;
